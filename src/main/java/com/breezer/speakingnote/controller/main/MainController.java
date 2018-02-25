21f4966e471563d0e8182321607233c1dd6f423e
SangHoon <SangHoon@SangHoon-PC>
2018-02-25 14:35:13 +0900

    Show Welcome page.
    remove sql, auth setting

--- src/main/java/com/breezer/speakingnote/controller/main/MainController.java
+++ src/main/java/com/breezer/speakingnote/controller/main/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class MainController {
 	
+	@RequestMapping("/")
 	public String main() {
 		return "main/index";
 	}
