c12c04a45ddb635796a6ee37600c18f16cbd393a
SangHoon <SangHoon@SangHoon-PC>
2018-02-25 14:21:06 +0900

    MainController init

---
+++ src/main/java/com/breezer/speakingnote/controller/main/MainController.java
@@ -0,0 +1,14 @@
+package com.breezer.speakingnote.controller.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("MainController")
+@RequestMapping("/")
+public class MainController {
+	
+	public String main() {
+		return "main/index";
+	}
+	
+}
